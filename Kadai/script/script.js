2d2a7028211f888a3526d82f67909daa116fcbbb
gutti-misdo <2301162@s.asojuku.ac.jp>
2024-12-02 23:51:25 +0900

    commit

--- Kadai/script/script.js
+++ Kadai/script/script.js
@@ -10,25 +10,37 @@ new Vue({
   methods: {
     validateName() {
       //名前は5文字以上で入力　5文字以下ならエラーメッセージ表示
-
+      if (this.name.length < 5) {
+        this.errorMessage = '名前は5文字以上で入力してください。';
+        this.isValidName = false;
+      } else {
+        this.errorMessage = '';
+        this.isValidName = true;
+      }
     },
     drawFortune() {
       // バリデーションが成功した場合のみランダムな運勢を生成
-      if () {
+      if (this.isValidName) {
         //0から100までのランダムな数値を取得する
         const randomNumber = Math.round(Math.random() * 100);
         //数値が80以上の場合（大吉）
-
-        //数値が60以上の場合（吉）
-
-        //数値が40以上の場合（中吉）
-
-        //数値が20以上の場合（小吉）
-
-        //それ以外の場合（凶）
-
-      }
+        if (randomNumber >= 80) {
+          this.fortune = 'daikichi';
+          //数値が60以上の場合（吉）
+        } else if (randomNumber >= 60) {
+          this.fortune = 'kichi';
+          //数値が40以上の場合（中吉）
+        } else if (randomNumber >= 40) {
+          this.fortune = 'chukichi';
+          //数値が20以上の場合（小吉）
+        } else if (randomNumber >= 20) {
+          this.fortune = 'shokichi';
+          //それ以外の場合（凶）
+        } else {
+          this.fortune = 'kyo';
+        }
       }
     }
   }
-});
+}
+);
